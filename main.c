359cee127c420bdff7b1972f099d60999b54f154
minaalbert33 <mina.albert33@gmail.com>
2024-07-10 13:52:19 +0300

    Add parse_input func

--- main.c
+++ main.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-#define MAX_INPUT_LENGTH 1024
+#define MAX_INPUT_LENGTH 2048
+/* functions declarations*/
+void read_input(char *);
+int parse_input(char **, char *);
+void execute_command(char **);
 
+/* functions definitions*/
 void
-read_input(char *input_buffer)
-{
+read_input(char *input_buffer) {
     char PWD[MAX_INPUT_LENGTH];
     char *pwd = getcwd(PWD, sizeof(PWD));
 
+    fflush(stdout);
     printf("%s > ", pwd);
     if(fgets(input_buffer, MAX_INPUT_LENGTH, stdin) == NULL){
         perror("ERROR");
@@ -22,13 +28,57 @@ read_input(char *input_buffer)
 }
 
 int
-main() {
-    while (1) {
-        char input_buffer[MAX_INPUT_LENGTH];
-        read_input(input_buffer);
+parse_input(char **argv, char *input_buffer){
+    char *token = strtok(input_buffer, " ");
+    int argc = 0;
+    while (token != NULL) {
+        argv[argc] = token;
+        token = strtok(NULL, " ");
+        argc++;
+    }
 
-        if(strcmp(input_buffer, "exit") == 0)
-           exit(EXIT_SUCCESS);
+    return argc;
+
+}
+
+void
+execute_command(char **argv){
+    if(strcmp(argv[0], "exit") == 0)
+       exit(EXIT_SUCCESS);
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(1);
+    } else if (pid == 0) {
+
+        if (execvp(argv[0], argv) == -1) 
+            perror("Execution failed");
+
+        exit(1);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+}
+
+int main() {
+    char input_buffer[MAX_INPUT_LENGTH];
+    char *argv[MAX_INPUT_LENGTH];
+    int argc = 0;
+
+    while (1) {
+        read_input(input_buffer);
+        argc = parse_input(argv, input_buffer);
+        // printf("argc = %d\n", argc);
+        //
+        // for (int i = 0; i < argc; i++) {
+        //     printf("argv[%d] = %s\n", i, argv[i]);
+        // }
+
+        if (argc > 0) {
+            execute_command(argv);
+        }
 
     }
     return EXIT_SUCCESS;
